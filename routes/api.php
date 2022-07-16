3650df3a3cfd1d37960decd7abee184863b97d57
indiesurya <putuindie11@gmail.com>
2022-07-16 09:56:56 +0800

    add routing logout and show profile user authenticated

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function (Request $request) {
+        return auth()->user();
+    });
+    Route::post('logout', [AuthenticationController::class, 'logout']);
 });
 
 Route::post('register',[AuthenticationController::class, 'register']);
