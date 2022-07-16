306d820e117000c499dd775ff682e57a1177b820
indiesurya <putuindie11@gmail.com>
2022-07-16 09:14:51 +0800

    add simple login api

--- app/Http/Controllers/API/AuthenticationController.php
+++ app/Http/Controllers/API/AuthenticationController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticationController extends Controller
 {
@@ -61,6 +62,19 @@ class AuthenticationController extends Controller
         }
     }
 
+    public function login(Request $request){
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()
+                ->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json(['message' => 'Hi ' . $user->name . ', welcome to home', 'access_token' => $token, 'token_type' => 'Bearer',]);
+    }
     /**
      * Store a newly created resource in storage.
      *
